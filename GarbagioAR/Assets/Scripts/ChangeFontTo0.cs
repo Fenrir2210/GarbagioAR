9d8f8f089aeecd96ff3b990b69eba77032586666
mlaird6 <mlaird6@uwo.ca>
2019-03-30 15:02:50 -0400

    starting the textchange and colourchange w globals

---
+++ GarbagioAR/Assets/Scripts/ChangeFontTo0.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChangeFontTo0 : MonoBehaviour
+{
+
+    private void Start()
+    {
+        int CurrentFont = Globals.FontSize;
+        Debug.Log("oi the current font size is: " + CurrentFont);
+
+        Button thisButton = this.GetComponent<Button>();
+        thisButton.onClick.AddListener(SceneChange);
+    }
+
+    private void SceneChange()
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+}
---
+++ GarbagioAR/Assets/Scripts/Globals.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Globals
+{
+    public static int FontSize{
+        get{
+            return FontSize;
+        }
+        set{
+            FontSize = value;
+        }
+    }
+    public static Color mainColor {
+        get{
+            return mainColor;
+        }
+        set{
+            mainColor = value;
+        }
+    }
+    public static Color topColor {
+        get{
+            return topColor;
+        }
+        set{
+            topColor = value;
+        }
+    }
+    
+}
+
