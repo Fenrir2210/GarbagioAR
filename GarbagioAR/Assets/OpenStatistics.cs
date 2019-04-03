6f145d796588338dd30774b7fdf62085486b1f68
Spencer <spencerm133@gmail.com>
2019-04-03 14:10:37 -0400

    started statistics branch

---
+++ GarbagioAR/Assets/OpenStatistics.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class OpenStatistics : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Button thisButton = this.GetComponent<Button>();
+        thisButton.onClick.AddListener(SceneChange);
+    }
+
+    // Update is called once per frame
+    private void SceneChange ()
+    {
+        SceneManager.LoadScene("StatisticsScene", LoadSceneMode.Single);
+    }
+}
