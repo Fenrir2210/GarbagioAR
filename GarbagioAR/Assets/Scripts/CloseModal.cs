1546c8c278b1077b5f9cde26a807635bb37a7a50
Fenrir2210 <dljeti@uwo.ca>
2019-04-06 17:19:09 -0400

    AR View Done + Modal Scripts

---
+++ GarbagioAR/Assets/Scripts/CloseModal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CloseModal : MonoBehaviour
+{
+    private GameObject modalCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        modalCanvas = this.transform.parent.gameObject;
+        Button thisButton = this.GetComponent<Button>(); //The exit button itself
+        thisButton.onClick.AddListener(exitModal);
+    }
+
+    // Update is called once per frame
+    private void exitModal()
+    {
+        modalCanvas.SetActive(false);
+    }
+}
---
+++ GarbagioAR/Assets/Scripts/OpenModal.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenModal : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.collider.tag == "Quad")
+            {
+                //hit.collider.gameObject now refers to the 
+                //quad under the mouse cursor if present
+                if (Input.GetMouseButtonDown(0))
+                    GameObject.Find("DetailModalCanvas").SetActive(true);
+
+
+            }
+        }
+    }
+}
