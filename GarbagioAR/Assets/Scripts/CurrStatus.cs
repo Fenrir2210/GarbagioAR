a5334620783d03988ac478ea2be3ba85c4fac0d0
mlaird6 <mlaird6@uwo.ca>
2019-04-06 14:38:59 -0400

    currently not working, scaling setting changes to other scenes

---
+++ GarbagioAR/Assets/Scripts/CurrStatus.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CurrStatus : MonoBehaviour
+{
+    static CurrStatus status;
+    public Color32 BackgroundColour = new Color32(75, 102, 161, 255);
+    public Color32 HeaderColour = new Color32(51, 72, 130, 255);
+    public int FontSize = 22;
+  
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("STARTING");
+        Debug.Log("status=" + status);
+        //if (status != null)
+        //{
+        //    //there is already an instance of font size, self destory
+        //    Destroy(this.gameObject);
+        //    Debug.Log("object destoryed: " + this.gameObject);
+        //    return;
+        //}
+        GameObject go = GameObject.Find("CurrStatus");
+        status = this;
+        DontDestroyOnLoad(go);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ChangeFont(int NewFontSize)
+    {
+        FontSize = NewFontSize;
+    }
+
+    public void ChangeBackgroundColour(string NewBackgroundColourString)
+    {
+        ColourExtensions col = new ColourExtensions();
+        Color32 NewColour = col.FromString(NewBackgroundColourString);
+        BackgroundColour = NewColour;
+        Debug.Log("Oi cunt want soem fuck?!?! ONe");
+    }
+
+    public void ChangeHeaderColour(string NewHeaderColourString)
+    {
+        ColourExtensions col = new ColourExtensions();
+        Color32 NewColour = col.FromString(NewHeaderColourString);
+        HeaderColour = NewColour;
+        Debug.Log("Oi cunt want soem fuck?!?! TWO");
+    
+
+    }
+
+    //private void OnDestroy()
+    //{
+    //    Debug.Log("CurrStatus was destroyed");
+    //}
+
+    public void LoadScene(string SceneName)
+    {
+        SceneManager.LoadScene(SceneName);
+
+    }
+}
---
+++ GarbagioAR/Assets/Scripts/Updater.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Updater : MonoBehaviour
+{
+    public GameObject BackgroundColourOb;
+    public GameObject HeaderColourOb;
+    public GameObject TextOb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject go = GameObject.Find("CurrStatus");
+        if (go == null)
+        {
+            Debug.LogError("Failed to find an object named CurrStatus");
+            this.enabled = false;
+            return;
+        }
+        CurrStatus cs = go.GetComponent<CurrStatus>();
+        TextOb.GetComponent<Text>().fontSize = cs.FontSize;
+        BackgroundColourOb.GetComponent<Image>().color = cs.BackgroundColour;
+        HeaderColourOb.GetComponent<Image>().color = cs.HeaderColour;
+    }
+}
