85599266586a6b413db17aeb20109bc916316c34
Fenrir2210 <dljeti@uwo.ca>
2019-04-07 16:56:05 -0400

    Added rumble on scan

---
+++ GarbagioAR/Assets/rumble.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class rumble : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>(); // On start we attach the behaviour to this gameObject
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    public void OnTrackableStateChanged(TrackableBehaviour.Status previousStatus, TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED) //if something is detected to be tracked . . . 
+        {
+            Handheld.Vibrate(); // vibrate the phone
+        }
+    }
+}
